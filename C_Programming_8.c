9214dd86c6cc9e74e8fe5bd3c470b0554325d316
Riya152005 <riyarathna1507@gmail.com>
2026-06-20 18:27:50 +0530

    Create C_Programming_8.c

---
+++ C_Programming_8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int b[5], a[5], i, j = 0;
+    printf("Enter 5 numbers:\n");
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(i = 0; i < 5; i++)
+    {
+        if(a[i] % 2 == 0)
+        {
+            b[j] = a[i];
+            j++;
+        }
+    }
+    printf("New array:\n");
+    for(i = 0; i < j; i++)
+    {
+        printf("%d ", b[i]);
+    }
+}
