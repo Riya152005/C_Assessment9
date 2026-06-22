bcc0212dc8b5a38af7423982b598d73be8560957
Riya152005 <riyarathna1507@gmail.com>
2026-06-22 18:52:26 +0530

    Create C_Programming_16.c

---
+++ C_Programming_16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int a[50], b[50], c[51], i, sum, carry = 0;
+    printf("Enter first 50-digit number:\n");
+    for(i = 0; i < 50; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter second 50-digit number:\n");
+    for(i = 0; i < 50; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    for(i = 49; i >= 0; i--)
+    {
+        sum = a[i] + b[i] + carry;
+        c[i + 1] = sum % 10;
+        carry = sum / 10;
+    }
+    c[0] = carry;
+    printf("Result:\n");
+    for(i = 0; i < 51; i++)
+    {
+        printf("%d", c[i]);
+    }
+}
